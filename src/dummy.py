a25ace4f1380e2016096f34979f590ccba317266
Mihai-Cristian Olteanu <mihaicristian.olteanu98@gmail.com>
2026-08-04 04:18:07 +0300

    Cleanup on the Google Authentication flow

--- src/dummy.py
+++ src/dummy.py
@@ -1,31 +1,57 @@
 import os
+import argparse
+import logging
 from pathlib import Path
 
 from google.auth import default
 import gspread
 
-GOOGLE_SPREADSHEET_ID = "1Dnh4blW2lAa9PRYKVJL1i79JZCDc6V0Uzw2ZRR9P10Y"
-GOOGLE_SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets.readonly",
-    "https://www.googleapis.com/auth/drive.readonly",
-]
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+
+logger = logging.getLogger(__name__)
 
 
-def google_authentication():
-    if not os.getenv("GITHUB_ACTIONS"):
-        # Running locally
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(Path("service-account-credentials.json").absolute())
+class GoogleSheets:
+    SPREADSHEET_ID = "1Dnh4blW2lAa9PRYKVJL1i79JZCDc6V0Uzw2ZRR9P10Y"
+    SCOPES = [
+        "https://www.googleapis.com/auth/spreadsheets.readonly",
+        "https://www.googleapis.com/auth/drive.readonly",
+    ]
 
-    creds, _ = default(scopes=GOOGLE_SCOPES)
-    return creds
+    @classmethod
+    def google_authentication(cls):
+        if not os.getenv("GITHUB_ACTIONS"):
+            # Running locally
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(Path("service-account-credentials.json").absolute())
+
+        creds, _ = default(scopes=cls.SCOPES)
+        return creds
+
+    @classmethod
+    def get_spreadsheet(cls, worksheet_name="Form Responses 1"):
+        creds = cls.google_authentication()
+        gc = gspread.authorize(creds)
+        spreadsheet = gc.open_by_key(cls.SPREADSHEET_ID).worksheet(worksheet_name)
+
+        return spreadsheet
+
+
+def parse_args():
+    logger.info("Parsing arguments . . .")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--db-path", type=Path, default=Path("output/translators.json"))
+    parser.add_argument("-s", "--save-path", type=Path, default=Path("output/translators_filtered.json"))
+    args = parser.parse_args()
+
+    return args
 
 
 def main():
-    creds = google_authentication()
-    gc = gspread.authorize(creds)
-    sheet = gc.open_by_key(GOOGLE_SPREADSHEET_ID).worksheet("Form Responses 1")
+    sheet = GoogleSheets.get_spreadsheet()
+
     rows = sheet.get_all_records()
-    print(rows)
+    # print(rows[:10])
 
 
 if __name__ == "__main__":
